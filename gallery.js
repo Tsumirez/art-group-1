2b1e4f08cd3e7336f17ff2d45e4531ea1f1af9cc
Tsumirez <tsumirez@zoho.com>
2021-09-27 11:11:05 +0200

    Commentary for js gallery file, small fixes for css file.

--- gallery.js
+++ gallery.js
@@ -1,7 +1,10 @@
+//pobranie slidów z galerii celem przypisania im kolejności wyświetlania
 let slides = document.querySelectorAll(".slides > div");
 
+//przypisanie domyślnej kolejności (poprzez właściwość order w css)
 slides.forEach((slide, index) => (slide.style.order = index));
 
+//even handler dla nawigacyjnych strzałek. Przypisuje nowy porządek dla każdego slajdu.
 let browse = (offset) => {
   slides.forEach((slide, index) => {
      slide.style.order = ((+slide.style.order+offset) % slides.length +1 || slides.length)-1;
